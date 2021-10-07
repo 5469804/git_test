377cbaabbd9eeccbb6e891218e240847e6548861
승현 이 <sh980812@naver.com>
2021-10-07 20:20:30 +0900

    느낌표 두개 추가

--- git_test/Form1.cs
+++ git_test/Form1.cs
@@ -19,7 +19,7 @@ namespace git_test
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World");
+            MessageBox.Show("Hello World!!");
         }
     }
 }
